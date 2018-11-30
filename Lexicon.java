0adb0ba134d6b515f94ef116b61d5feffd839741
Philipp <pja21393@gmail.com>
2018-11-30 00:26:51 -0500

    first commit

---
+++ Lexicon.java
@@ -0,0 +1,140 @@
+import java.util.Arrays;
+
+class Lexicon
+{
+  private int[] hashTable;
+  private char[] wordArray;
+  private int sizeFactor = 15;
+  private int size = -1;
+  private int insertPosition = 0;
+
+  public Lexicon(int size)
+  {
+    this.size = size;
+
+    hashTable = new int[size];
+    wordArray = new char[sizeFactor*size];
+
+    Arrays.fill(hashTable, -1);
+    Arrays.fill(wordArray, ' ');
+  }
+
+  public boolean empty()
+  {
+    int hashTableLength = hashTable.length;
+
+    for(int i = 0; i < hashTableLength; i++)
+    {
+      if(hashTable[i] != -1)
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public void insert(String word)
+  {
+    int hPrime = hPrime(size, word);
+    int insertIndex = -1;
+
+    int i = 0;
+    while(i < size)
+    {
+      insertIndex = (hPrime + i*i) % size;
+      if(hashTable[insertIndex] == -1)
+      {
+        hashTable[insertIndex] = insertPosition;
+
+        for(int j = 0; j < word.length(); j++)
+        {
+          wordArray[insertPosition++] = word.charAt(j);
+        }
+        wordArray[insertPosition++] = '\\';
+
+        break;
+      }
+      i++;
+    }
+  }
+
+  //returns hashTableIndex if found or -1 if not found
+  public int search(String word)
+  {
+    int hPrime = hPrime(size, word);
+    int insertIndex = -1;
+    int startingIndex = -1;
+
+    int i = 0;
+    while(i < size)
+    {
+      insertIndex = (hPrime + i*i) % size;
+      startingIndex = hashTable[insertIndex];
+
+      if(startingIndex != -1 && wordArray[startingIndex + word.length()] == '\\')
+      {
+          for(int j = 0; j < word.length(); j++)
+          {
+            if(word.charAt(j) != wordArray[startingIndex + j])
+            {
+              break;
+            }
+            return insertIndex;
+          }
+      }
+      i++;
+    }
+    return -1;
+  }
+
+  public int delete(String word)
+  {
+    int hashTableIndexOfWord = search(word);
+    if(hashTableIndexOfWord != -1)
+    {
+      int startingIndex = hashTable[hashTableIndexOfWord];
+      while(wordArray[startingIndex] != '\\')
+      {
+        wordArray[startingIndex++] = '*';
+      }
+      hashTable[hashTableIndexOfWord] = -1;
+    }
+    return hashTableIndexOfWord;
+  }
+
+  private int hPrime(int modInt, String word)
+  {
+    int sum = sumOfChar(word);
+
+    return sum % modInt;
+  }
+
+  private int sumOfChar(String word)
+  {
+    int sum = 0;
+    for(int i = 0; i < word.length(); i++)
+    {
+      sum += (int) word.charAt(i);
+    }
+    return sum;
+  }
+
+  public void debugPrint()
+  {
+    System.out.print("T\tA: ");
+
+    String words = "";
+    for(int i = 0; i < wordArray.length; i++)
+    {
+      System.out.print(wordArray[i]);
+    }
+    System.out.println();
+
+    for(int i = 0; i < hashTable.length; i++)
+    {
+      System.out.println(i+ ": " +(hashTable[i] == -1 ? "" : hashTable[i]));
+    }
+    System.out.println();
+  }
+}
---
+++ Tester.java
@@ -0,0 +1,22 @@
+class Tester
+{
+  public static void main(String args[])
+  {
+    Lexicon l = new Lexicon (11);
+
+    l.insert("alex");
+    l.insert("tom");
+    l.insert("jerry");
+    l.debugPrint();
+
+    System.out.println(l.search("alex"));
+    System.out.println(l.search("tom"));
+    System.out.println(l.search("jerry"));
+    System.out.println(l.search("mary"));
+    System.out.println(l.delete("tom"));
+    System.out.println(l.delete("aley"));
+    System.out.println(l.delete("alex"));
+    System.out.println(l.delete("jerry"));
+    l.debugPrint();
+  }
+}
