0047e9fb6b4be2f5f353fd27216469da72c4d38a
Philipp <pja21393@gmail.com>
2018-11-30 17:23:34 -0500

    removed function tests

--- Tester.java
+++ Tester.java
@@ -4,41 +4,6 @@ class Tester
 {
   public static void main(String args[])
   {
-    /*
-    Lexicon l = new Lexicon (1);
-
-    l.insert("thisisareallylongwordthatisntreallyawordbutjustfortesting");
-    l.debugPrint();
-    l.delete("alex");
-    l.debugPrint();
-
-    l.insert("tom");
-    l.debugPrint();
-    l.insert("jerry");
-    l.debugPrint();
-
-    l.delete("tom");
-    l.debugPrint();
-    l.delete("jerry");
-    l.debugPrint();
-    l.insert("jerry");
-    l.debugPrint();
-    l.insert("tom");
-    l.debugPrint();
-    l.delete("jerry");
-    l.debugPrint();
-    l.insert("tom");
-    l.insert("alex");
-    l.insert("tom");
-    l.insert("tom");
-    l.delete("thisisareallylongwordthatisntreallyawordbutjustfortesting");
-
-    l.debugPrint();
-    System.out.println(l.search("tom"));
-    System.out.println(l.search("aley"));
-    System.out.println(l.search("alex"));
-    */
-
     batch("test.txt");
   }
 
