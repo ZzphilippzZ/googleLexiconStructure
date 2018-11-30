3dee37f9e47e313be6271fe41eef7197ec73fea5
Philipp <pja21393@gmail.com>
2018-11-30 17:17:40 -0500

    added batch()

--- Tester.java
+++ Tester.java
@@ -1,7 +1,10 @@
+import java.io.*;
+
 class Tester
 {
   public static void main(String args[])
   {
+    /*
     Lexicon l = new Lexicon (1);
 
     l.insert("thisisareallylongwordthatisntreallyawordbutjustfortesting");
@@ -34,5 +37,75 @@ class Tester
     System.out.println(l.search("tom"));
     System.out.println(l.search("aley"));
     System.out.println(l.search("alex"));
+    */
+
+    batch("test.txt");
+  }
+
+  public static void batch(String filename)
+  {
+    try(BufferedReader br = new BufferedReader(new FileReader(filename)))
+    {
+      String currentLine = "";
+      String[] lineTokens;
+      Lexicon l = null;
+
+      while((currentLine = br.readLine()) != null)
+      {
+        String token2 = "";
+        lineTokens = currentLine.split(" ");
+
+        if(lineTokens.length == 2)
+        {
+          token2 = lineTokens[1];
+        }
+
+        int result = 0;
+        String output = "";
+        int operation = Integer.parseInt(lineTokens[0]);
+        switch (operation)
+        {
+          case 10:
+            l.insert(token2);
+            break;
+          case 11:
+            result = l.delete(token2);
+            if(result < 0)
+            {
+              output = token2+ " not found";
+            }
+            else
+            {
+              output = token2+ " deleted from slot " +result;
+            }
+            System.out.println(output);
+            break;
+          case 12:
+            result = l.search(token2);
+            if(result < 0)
+            {
+              output = token2+ " not found";
+            }
+            else
+            {
+              output = token2+ " found at slot " +result;
+            }
+            System.out.println(output);
+            break;
+          case 13:
+            l.debugPrint();
+            break;
+          case 14:
+            l = new Lexicon(Integer.parseInt(token2));
+            break;
+          default:
+            System.out.println("Case " +operation+ " does not exist");
+        }
+      }
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
   }
 }
